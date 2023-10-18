0a95dc49b5c7f8c9c679b758a45e76c5bd05116d
Dylan Bautista <dylanbaucarc9@gmail.com>
2023-10-18 19:48:31 -0300

    carrito y navbar

---
+++ src/Components/Carrito/Carrito.js
@@ -0,0 +1,13 @@
+import './Carrito.css';
+const CarritoC = () => {
+    const counter = 0;
+    return(
+        <div className='div-carrito'>
+            <img className='foto-carrito' src="https://cdn-icons-png.flaticon.com/512/107/107831.png" alt="MDN" />
+            <h2>{counter}</h2>
+            </div>
+       
+
+    );
+}
+export default CarritoC
---
+++ src/Components/Navegacion/Navegacion.js
@@ -0,0 +1,12 @@
+import './Navegacion.css';
+const MiComponente = () => {
+    return (
+        <ul className='maxlist'>
+            <li className='list'><a href="#">Contacto</a></li>
+            <li className='list'><a href="#">Soporte</a></li>
+            <li className='list'><a href="#">Tienda</a></li>
+            <li className='list'><a href="#">Home</a></li>
+        </ul>
+    );
+};
+export default MiComponente;
